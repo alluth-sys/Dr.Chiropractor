1279017d6560be5fa680fe0129d8c150e425eb6c
Albert Lu <lualbert88@gmail.com>
2021-07-27 00:00:32 +0800

    Add styles to Sign Up Screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,18 +3,18 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
 //Auth Flow
-import WelcomeScreen from "./screens/Auth/WelcomeScreen";
-import SigninScreen from "./screens/Auth/SigninScreen";
-import SignUpScreen from "./screens/Auth/SignUpScreen";
-import OTPScreen from "./screens/Auth/OTPScreen";
+import WelcomeScreen from "../screens/Auth/WelcomeScreen";
+import SigninScreen from "../screens/Auth/SigninScreen";
+import SignUpScreen from "../screens/Auth/SignUpScreen";
+import OTPScreen from "../screens/Auth/OTPScreen";
 
 //Main Flow
-import Account from "./screens/Main/Account";
-import HomeScreen from "./screens/Main/HomeScreen";
-import Appointment from "./screens/Main/AppointmentScreen";
-import AppointmentDetail from "./screens/Main/AppointmentDetail";
-import SearchScreen from "./screens/Main/SearchScreen";
-import TrainerDetailScreen from "./screens/Main/TrainerDetailScreen";
+import Account from "../screens/Main/Account";
+import HomeScreen from "../screens/Main/HomeScreen";
+import Appointment from "../screens/Main/AppointmentScreen";
+import AppointmentDetail from "../screens/Main/AppointmentDetail";
+import SearchScreen from "../screens/Main/SearchScreen";
+import TrainerDetailScreen from "../screens/Main/TrainerDetailScreen";
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
--- screens/Auth/SignUpScreen.js
+++ screens/Auth/SignUpScreen.js
@@ -1,18 +1,86 @@
-import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import { Button } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 import SafeAreaView from "react-native-safe-area-view";
+import Spacer from "../../components/Spacer";
 
-const SignUpScreen = (props) => {
+const SignUpScreen = ({ navigation }) => {
+  //console.log(navigation);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <SafeAreaView>
+    <KeyboardAvoidingView style={styles.container}>
       <View>
-        <Text>SignUpScreen</Text>
+        <Text style={styles.textStyle}>
+          Your phone have been confirmed{"\n"}Let us know more about you
+        </Text>
+        <Spacer />
+        <View>
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            autoCorrect={false}
+          />
+        </View>
+        <Spacer />
+        <View>
+          <Text style={styles.label}>Password:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
+        </View>
+
+        <Spacer />
+        <Button title="Sign Up" />
       </View>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "opensans_bold",
+  },
+  input: {
+    height: 50,
+    width: 300,
+    padding: 5,
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "#F5F5F5",
+  },
+  label: {
+    marginBottom: 10,
+    fontSize: 15,
+    fontFamily: "opensans_bold",
+  },
+});
+
+SignUpScreen.navigationOptions = () => {
+  return {
+    title: "Sign Up",
+    headerLeft: () => null,
+  };
+};
 
 export default SignUpScreen;
